a08dac73f08f2c91fd331985ba5e31ad781d0485
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-04 10:35:55 -0300

    Update

--- Mapa_navegavel/js/script.js
+++ Mapa_navegavel/js/script.js
@@ -1,3 +1,23 @@
+
 var cnv = document.querySelector("canvas");
 var ctx = cnv.getContext("2d");
-alert(ctx);
+var sercX = 0;
+var sercY = 0;
+var size = 400;
+var map = new Image();
+map.src = "img/map.png";
+map.onload = render();
+
+
+function render(){
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+    ctx.drawImage(map, 0, 0, 400, 400, 0, 0, cnv.width, cnv.height);
+}
+
+function update(){
+
+}
+
+function looping(){
+
+}
