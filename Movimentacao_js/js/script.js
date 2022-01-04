79317f7102b67d5f908233def6adb8422c86c2f6
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-04 00:21:47 -0300

    keydownHandler

--- Movimentacao_js/js/script.js
+++ Movimentacao_js/js/script.js
@@ -1,12 +1,17 @@
 
 var cnv = document.querySelector("canvas");
 var ctx = cnv.getContext("2d");
-//
 
 var p1 = {
     x: 10,
     y: 10
 };
+
+window.addEventListener("keydown", keydownHandler);
+
+function keydownHandler(e){
+    alert(e.keyCode);
+}
 
 render();
 
