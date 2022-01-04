48c473ecdf469050199c315860be8bd40d8e73bd
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-03 23:25:01 -0300

    keyupHandler

--- js/script.js
+++ js/script.js
@@ -1,18 +1,76 @@
+
+//Entradas
+var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
+var SIZE = 50;
 var cnv = document.querySelector("canvas");
 var ctx = cnv.getContext("2d");
+var posX = 50;
+var posY = 50;
+var objColor = "#00f";
+
+var mvLeft = mvUp = mvRight = mvDown = false;
+
+function updateBlock(){
+    if(mvLeft){
+        posX--;
+    }
+    if(mvRight){
+        posX++;
+    }
+    if(mvUp){
+        posY--;
+    }
+    if(mvDown){
+        posY++;
+    }
+}
 
 window.addEventListener("keydown", keydownHandler, false);
+window.addEventListener("keyup", keyupHandler, false);
+
+function keyupHandler(e){
+    var key = e.keyCode;
+    switch(key){
+        case UP:
+            mvUp = false;
+            break;
+        case DOWN:
+            mvDown = false;
+            break;
+        case LEFT:
+            mvLeft = false;
+            break;
+        case RIGHT:
+            mvRight = false;
+            break;            
+    }
+}
 
 function keydownHandler(e){
-    alert(e.keyCode);
+    var key = e.keyCode;
+    switch(key){
+        case UP:
+            mvUp = true;
+            break;
+        case DOWN:
+            mvDown = true;
+            break;
+        case LEFT:
+            mvLeft = true;
+            break;
+        case RIGHT:
+            mvRight = true;
+            break;            
+    }
 }
 
 function update(){
-    
+    updateBlock();
 }
 
 function draw(){
-    ctx.fillRect(50, 50, 50, 50);
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+    ctx.fillRect(posX, posY, SIZE, SIZE);
 }
 
 
