64cb185a80f8a0cce337c86000c37133485f0477
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-04 22:34:36 -0300

    Movimentação

--- Colisao_dois/js/script.js
+++ Colisao_dois/js/script.js
@@ -16,7 +16,52 @@
 
     //Objstos Personagem
     var character = new Sprite(50, 175, 50, 50, "#00f");
+    character.speed = 2;
     sprites.push(character);
+
+    var block1 = new Sprite(500, 100, 50, 50, "#f00");
+    sprites.push(block1);
+
+    var block2 = new Sprite(200, 300, 100, 50, "#8B6914");
+    sprites.push(block2);
+
+
+    //Entradas
+    window.addEventListener("keydown", function(e){
+        var key = e.keyCode;
+        switch(key){
+            case UP:
+                mvUp = true;
+                break;
+            case DOWN:
+                mvDown = true;
+                break;
+            case LEFT:
+                mvLeft = true;
+                break;
+            case RIGHT:
+                mvRight = true;
+                break;            
+        }
+    }, false);
+
+    window.addEventListener("keyup", function(e){
+        var key = e.keyCode;
+        switch(key){
+            case UP:
+                mvUp = false;
+                break;
+            case DOWN:
+                mvDown = false;
+                break;
+            case LEFT:
+                mvLeft = false;
+                break;
+            case RIGHT:
+                mvRight = false;
+                break;            
+        }
+    }, false);
 
     //Funções
     function loop(){
@@ -27,7 +72,18 @@
 
 
     function update(){
-        
+        if(mvLeft && !mvRight){
+            character.posX -= character.speed;
+        }
+        if(mvRight && !mvLeft){
+            character.posX += character.speed;
+        }
+        if(mvUp && !mvDown){
+            character.posY -= character.speed;
+        }
+        if(mvDown && !mvUp){
+            character.posY += character.speed;
+        }
     }
 
 
