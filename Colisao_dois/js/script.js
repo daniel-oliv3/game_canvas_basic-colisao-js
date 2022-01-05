7286378100d389e528d425a40423333e991d5c62
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-04 21:54:23 -0300

    Update

--- Colisao_dois/js/script.js
+++ Colisao_dois/js/script.js
@@ -15,8 +15,34 @@
     var blocks = [];
 
     //Objstos Personagem
-    var character = new Sprite();
+    var character = new Sprite(50, 175, 50, 50, "#00f");
+    sprites.push(character);
 
+    //Funções
+    function loop(){
+        window.requestAnimationFrame(loop, cnv);
+        update();
+        render();
+    }
+
+
+    function update(){
+        
+    }
+
+
+    function render(){
+        ctx.clearRect(0, 0, cnv.width, cnv.height);
+        for(var i in sprites){
+            var spr = sprites[i];
+            if(spr.visible){
+                ctx.fillStyle = spr.color;
+                ctx.fillRect(spr.posX, spr.posY, spr.width, spr.height);
+            }
+        }
+    }
+
+    loop();
 
 
 
